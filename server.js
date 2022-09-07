928f847d04706a26188dc7153cc7b577e3dc76e8
Ann Pham <annpham001@gmail.com>
2022-09-07 15:57:03 +0930

    added react app in client folder

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000; 
 
-app.use(express.static('client'));
+app.use(express.static('client/build'));
 
 app.listen(port, () => {
     console.log(`listening at http://localhost:${port}`);
